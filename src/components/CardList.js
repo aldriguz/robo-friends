0d8e818cf2077ac4f080a6cf1b079a96511f9a6e
Kevin Alama <alama1992@gmail.com>
2020-09-29 09:04:25 -0500

    deploy to github

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from '../components/Card';
 
 const CardList = ({ robots }) => {
-    if(true){
+    /*if(true){
         throw new Error('NOOOO');
-    }
+    }*/
 
     const cardsArray = robots.map((user, i) => {
         return <Card key={i} id={user.id} name={user.name} email={user.email}/>
