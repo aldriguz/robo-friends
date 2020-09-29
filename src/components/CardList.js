9d9567fec195dc788d73e3a92bb3fff90162a347
Kevin Alama <alama1992@gmail.com>
2020-09-29 08:58:26 -0500

    adding boundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from '../components/Card';
 
 const CardList = ({ robots }) => {
+    if(true){
+        throw new Error('NOOOO');
+    }
+
     const cardsArray = robots.map((user, i) => {
         return <Card key={i} id={user.id} name={user.name} email={user.email}/>
     });
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 //import { robots } from './robots';
 import './App.css';
 
@@ -46,7 +47,9 @@ class App extends Component {
                     <h1 className='f1'>Robofriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                     
                 </div>
