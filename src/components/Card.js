b2e351a75febe212d71c57653f6bd73027ab7454
Kevin Alama Rodriguez <kevinalama@multiplica.com>
2021-01-16 01:04:44 -0500

    issues fixed for performance

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const {name, id, email} = props; //deconstructing
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+            <img alt='robot' src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
